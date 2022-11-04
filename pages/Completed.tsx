8da44f7e50c6de0fb9aa4ed169451f8bd57ef7a6
KruzadeR-VictoR <victorbiju31@gmail.com>
2022-11-04 13:12:52 +0530

    mod-1, input functionality not added

---
+++ pages/Completed.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Completed = (props: Props) => {
+  return (
+    <div>Completed</div>
+  )
+}
+
+export default Completed
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,60 +13,126 @@ import Container from "@mui/material/Container";
 import Checkbox from "@mui/material/Checkbox";
 import TextField from "@mui/material/TextField";
 
+type Todo = {
+  id: number;
+  text: string;
+  isComplete: boolean;
+};
+
 export default function Home() {
   const [isActive, setisActive] = useState(false);
-  const [isComplete, setisComplete] = useState(false)
+  const [TodoMenu, setTodoMenu] = useState("All");
+  const [isComplete, setisComplete] = useState(false);
   // const [Screen, setScreen] = useState(window)
-  const [Todos, setTodos] = useState([
+  const [Todos, setTodos] = useState<Todo[]>([
     {
       id: 1,
       text: "Complete online javascript course",
-      isComplete:isComplete,
+      isComplete: isComplete,
     },
     {
       id: 2,
       text: "Jog around the park 3x",
-      isComplete:isComplete,
+      isComplete: isComplete,
     },
     {
       id: 3,
       text: "10 minutes meditation",
-      isComplete:isComplete,
+      isComplete: isComplete,
     },
     {
       id: 4,
       text: "Read for 1 hour",
-      isComplete:isComplete,
+      isComplete: isComplete,
     },
     {
       id: 5,
       text: "Pick up groceries",
-      isComplete:isComplete,
+      isComplete: isComplete,
     },
     {
       id: 6,
       text: "Complete Todo App on Frontend Mentor",
-      isComplete:isComplete,
+      isComplete: isComplete,
     },
   ]);
 
-  console.log(Todos[0]);
+  const [CompletedTodos, setCompletedTodos] = useState<Todo[]>([]);
+
+  const [Current, setCurrent] = useState<Todo[]>(Todos);
+
+  // console.log(Current);
 
   const todoref = useRef();
 
-  // useEffect(()=>{
-  //     console.log(window.innerWidth);
-  // },[])
+  useEffect(() => {
+    setTodos(Todos);
+    setCurrent(Todos);
+  }, [Todos]);
 
-  const handleCheckbox = (e: any,index:any) => {
+  const handleCheckbox = (e: any, index: any) => {
     setisActive((prev) => !prev);
-    console.log(index)
-    Todos.splice(index,1)
+    console.log(Todos);
+    // setisComplete(prev=>!prev)
+    Todos[index].isComplete
+      ? (Todos[index].isComplete = false)
+      : (Todos[index].isComplete = true);
+    // Todos.splice(index, 1);
+    const complete = Todos.filter((item) => {
+      return item.isComplete == true;
+    });
+    setCompletedTodos(complete);
   };
 
-  const handleClose = (e: any) => {
-    console.log(e);
-  };  
+  const handleClose = (e: any, index: any) => {
+    setisActive((prev) => !prev);
+    console.log(index);
+    Todos.splice(index, 1);
+  };
+
+  //> handle All Todos
+  const handleTodos = () => {
+    setTodoMenu("All");
+    setCurrent(Todos);
+  };
+  //> handle Completed Todos
+
+  useEffect(() => {
+    console.log(CompletedTodos);
+  });
+
+  const handleCompleted = () => {
+    setTodoMenu("Completed");
+    const complete = Todos.filter((item) => {
+      return item.isComplete == true;
+    });
+    setCompletedTodos(complete);
+    setCurrent(complete);
+  };
+  //> handle Active Todos
+  const handleActive = () => {
+    setTodoMenu("Active");
+    const complete = Todos.filter((item) => {
+      return item.isComplete !== true;
+    });
+    setCompletedTodos(complete);
+    setCurrent(complete);
+  };
+
+  useEffect(() => {
+    console.log(CompletedTodos);
+  }, [CompletedTodos]);
+
+  //> Clear Completed todos
+
+  const clearCompleted = () => {
+    // setisActive((prev) => !prev);
+    setTodos(
+      Todos.filter((item, index) => {
+        return item.isComplete !== true;
+      })
+    );    
+  };
 
   return (
     <main className={style.App}>
@@ -113,10 +179,7 @@ export default function Home() {
             marginTop: "2.5rem",
           }}
         >
-          <div
-            className={`${style.checkbox}`}            
-          >            
-          </div>
+          <div className={`${style.checkbox}`}></div>
           <input
             className={style.createInput}
             type="text"
@@ -125,39 +188,88 @@ export default function Home() {
         </Stack>
         {/* All Todos  */}
         <Box className={style.todos} ref={todoref}>
-          {Todos.map((todo, index) => (
-            <div key={todo.id} className={style.todo}>
-              <div className={style.todo_left}>
-                <div
-                  className={`${
-                    isActive
-                      ? `${style.checkbox} ${style.checkbox_active}`
-                      : `${style.checkbox}`
-                  }`}
-                  onClick={(e) => handleCheckbox(e,index)}
-                >
-                  {isActive && <Image src={check} alt="check" />}
+          {TodoMenu == "Completed" ? (
+            <>
+              {CompletedTodos.map((todo, index) => (
+                <div key={todo.id} className={style.todo}>
+                  <div className={style.todo_left}>
+                    <div
+                      className={`${
+                        todo.isComplete
+                          ? `${style.checkbox} ${style.checkbox_active}`
+                          : `${style.checkbox}`
+                      }`}
+                      onClick={(e) => handleCheckbox(e, index)}
+                    >
+                      {todo.isComplete && <Image src={check} alt="check" />}
+                    </div>
+                    <span
+                      className={`${
+                        todo.isComplete
+                          ? `${style.todo_text} ${style.complete}`
+                          : `${style.todo_text}`
+                      }`}
+                    >
+                      {todo.text}
+                    </span>
+                  </div>
+                  <Image
+                    className={style.close_Btn}
+                    src={close}
+                    alt="close"
+                    onClick={(e) => handleClose(e, index)}
+                  />
                 </div>
-                <span className={style.todo_text}>{todo.text}</span>
-              </div>
-              <Image
-                className={style.close_Btn}
-                src={close}
-                alt="close"
-                onClick={(e) => handleClose(e)}
-              />
-            </div>
-          ))}
+              ))}
+            </>
+          ) : (
+            <>
+              {Current.map((todo, index) => (
+                <div key={todo.id} className={style.todo}>
+                  <div className={style.todo_left}>
+                    <div
+                      className={`${
+                        todo.isComplete
+                          ? `${style.checkbox} ${style.checkbox_active}`
+                          : `${style.checkbox}`
+                      }`}
+                      onClick={(e) => handleCheckbox(e, index)}
+                    >
+                      {todo.isComplete && <Image src={check} alt="check" />}
+                    </div>
+                    <span
+                      className={`${
+                        todo.isComplete
+                          ? `${style.todo_text} ${style.complete}`
+                          : `${style.todo_text}`
+                      }`}
+                    >
+                      {todo.text}
+                    </span>
+                  </div>
+                  <Image
+                    className={style.close_Btn}
+                    src={close}
+                    alt="close"
+                    onClick={(e) => handleClose(e, index)}
+                  />
+                </div>
+              ))}
+            </>
+          )}
+
           <div className={style.items_info}>
             <div className="left">
               <span>{Todos.length}</span> items left
             </div>
-            <span className={style.right}>Clear Completed</span>
+            <span className={style.right} onClick={clearCompleted}>
+              Clear Completed
+            </span>
           </div>
         </Box>
 
         <Stack
-        className={style.todo_menu}
+          className={style.todo_menu}
           direction="row"
           spacing={3}
           sx={{
@@ -168,13 +280,37 @@ export default function Home() {
             borderRadius: "5px",
           }}
         >
-          <span className={style.link_active} >All</span>
-          <span>Active</span>
-          <span>Completed</span>
+          <span
+            className={`${TodoMenu == "All" ? `${style.link_active}` : ""}`}
+            onClick={handleTodos}
+          >
+            All
+          </span>
+          <span
+            className={`${TodoMenu == "Active" ? `${style.link_active}` : ""}`}
+            onClick={handleActive}
+          >
+            Active
+          </span>
+          <span
+            className={`${
+              TodoMenu == "Completed" ? `${style.link_active}` : ""
+            }`}
+            onClick={handleCompleted}
+          >
+            Completed
+          </span>
         </Stack>
-
         <p className={style.Drag_msg}>Drag and drop to reorder list</p>
       </Container>
     </main>
   );
 }
+
+// export const Completed=()=>{
+//   return(
+//     <>
+//     {Compl}
+//     </>
+//   )
+// }
